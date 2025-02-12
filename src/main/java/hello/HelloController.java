59c7a5d147ff071f1e003d6e6347bba94bfd5c78
Mahima kesharwani <88523118+mahima2601@users.noreply.github.com>
2025-02-13 04:47:29 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-         "Greetings from Springboot..!!!";
+       return "Greetings from Springboot..!!!";
     }
     
 }
